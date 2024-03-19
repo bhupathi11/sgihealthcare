5b1b29cf63a9eb13066f11b862d74e64f7e19a8f
bhupathi-mpi <bhupathi@metropufindustries.com>
2024-03-19 18:46:08 +0530

    added services

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 // Show or hide the button based on scroll position
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -16,3 +16,17 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+$(".sexytabs").tabs({
+  show: { effect: "slide", direction: "left", duration: 200, easing: "easeOutBack" },
+  hide: { effect: "slide", direction: "right", duration: 200, easing: "easeInQuad" }
+});
+
+$('body').on('mouseenter mouseleave', '.nav-item', function (e) {
+  if ($(window).width() > 750) {
+    var _d = $(e.target).closest('.nav-item'); _d.addClass('show');
+    setTimeout(function () {
+      _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+    }, 1);
+  }
+});
